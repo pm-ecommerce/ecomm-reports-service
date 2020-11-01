d9c033826c30ad8f59ac3fd23ed34f9819cbe008
duy-miu <65563657+duy-miu@users.noreply.github.com>
2020-11-02 02:28:11 +0700

    make docker image
    make docker image

--- src/main/java/com/pm/ecommerce/reports_service/ReportsServiceApplication.java
+++ src/main/java/com/pm/ecommerce/reports_service/ReportsServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.pm.ecommerce.entities"})
+@EnableDiscoveryClient
 public class ReportsServiceApplication {
 
     public static void main(String[] args) {
